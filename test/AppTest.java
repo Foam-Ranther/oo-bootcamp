d70cbeaeb993c65ea0eb7897c8bf3043965fff7e
Foam-Ranther <yashzzz321@gmail.com>
2026-05-07 11:10:15 +0530

    Intial setup

---
+++ test/AppTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+class AppTest {
+  @Test
+  void name() {
+    mock();
+  }
+}
